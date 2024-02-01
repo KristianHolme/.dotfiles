864d5638c2d8474a9b4a3ecc6a00578400ab0fb7
Kristian Holme <holme.kristian@gmail.com>
2024-02-01 11:57:12 +0100

    add zsh aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,26 @@
+alias chrome='google-chrome'
+alias vim='nvim'
+alias vi='nvim'
+
+#basic config
+function my_zsh_setup() {
+    autoload -U zsh-newuser-install
+    zsh-newuser-install -f
+}
+alias zshsetup='my_zsh_setup'
+
+
+#moving to dirs
+alias nvimcfg='cd ~/.config/nvim/'
+alias zshcfg='cd ~/.oh-my-zsh/custom/'
+alias proj='cd ~/Code/prosjekt-master/src'
+alias rapp='cd ~/Code/prosjekt-master/rapport'
+alias matdir='cd ~/matlab'
+alias i3cfg='cd ~/.config/i3'
+
+#SSH
+alias markov='ssh kholme@markov.math.ntnu.no'
+alias work='cd work/kholme'
+
+#Running programs
+alias projmatlab='matlab -nodesktop -nosplash -r "cd ~/Code/prosjekt-master/src/; mrstModule add prosjektOppgave"'
