ce361668129731fd0da92a12f08acfb1b44df1f3
Kristian Holme <holme.kristian@gmail.com>
2025-08-12 13:42:08 +0200

    simplify julia setup

--- omarchy-tweaks/bin/julia-setup.jl
+++ omarchy-tweaks/bin/julia-setup.jl
@@ -3,7 +3,7 @@
 using Pkg
 Pkg.activate()
 packages = ["Revise", "BenchmarkTools", "Cthulhu", "Debugger", "DrWatson",
-    "PkgTemplates", "ProgressMeter", "BasicAutoloads", "OhMyREPL"]
+    "PkgTemplates", "ProgressMeter", "BasicAutoloads", "OhMyREPL", "Reexport"]
 for p in packages
     try
         Pkg.add(p)
@@ -12,13 +12,3 @@ for p in packages
         @warn "Error installing $p" exception = (e, catch_backtrace())
     end
 end
-
-import Base.Filesystem: cp, mkpath, dirname
-
-src = joinpath(homedir(), ".dotfiles", "julia_config", "startup.jl")
-dest_dir = joinpath(homedir(), ".julia", "config")
-dest = joinpath(dest_dir, "startup.jl")
-
-mkpath(dest_dir)
-cp(src, dest; force=true)
-@info "Copied $src to $dest"
