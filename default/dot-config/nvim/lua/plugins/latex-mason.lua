dd2803ae4d8478c1fe9b4695add4eabad200a15d
Kristian Holme <holme.kristian@gmail.com>
2025-08-31 19:00:16 +0200

    formatting and formatting

--- default/dot-config/nvim/lua/plugins/latex-mason.lua
+++ default/dot-config/nvim/lua/plugins/latex-mason.lua
@@ -1,25 +1,27 @@
 return {
-  -- Mason configuration for LaTeX tools
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "texlab",         -- LaTeX LSP
-        "latexindent",    -- LaTeX formatter
-        -- Note: chktex is not available in Mason registry
-        -- You can install it system-wide with: sudo pacman -S texlive-core
-      },
-    },
-  },
-  
-  -- Treesitter configuration for LaTeX
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "latex",
-        "bibtex",
-      },
-    },
-  },
+	-- Mason configuration for LaTeX tools
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"texlab", -- LaTeX LSP
+				"latexindent", -- LaTeX formatter
+				"julia-lsp",
+				"prettier",
+			},
+		},
+	},
+	-- Treesitter configuration for LaTeX
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"latex",
+				"bibtex",
+				"julia",
+				"json",
+			},
+		},
+	},
 }
