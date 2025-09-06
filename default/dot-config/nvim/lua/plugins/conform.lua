4b27bec194c7bf4b8ac2beb426888cfec7dbd09d
Kristian Holme <holme.kristian@gmail.com>
2025-09-06 15:47:51 +0200

    remove runic config as runic is now in conform

--- default/dot-config/nvim/lua/plugins/conform.lua
+++ default/dot-config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,6 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters = {
-      runic = {
-        command = "julia",
-        args = { "--project=@runic", "--startup-file=no", "-e", "using Runic; exit(Runic.main(ARGS))" },
-      },
-    },
     formatters_by_ft = {
       julia = { "runic" },
       lua = { "stylua" },
