d79ec7cc02eac9ac939bd3e9920ad8466766a9a6
Kristian Holme <holme.kristian@gmail.com>
2025-10-21 09:44:35 +0200

    add command for inserting empty lines

--- default/dot-config/nvim/lua/config/keymaps.lua
+++ default/dot-config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,10 @@
-vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center" })
-vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center" })
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center" })
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center" })
 
 vim.keymap.set("n", "<localleader>c", function()
   require("treesitter-context").go_to_context(vim.v.count1)
 end, { silent = true, desc = "Go to Treesitter context" })
 
-
+-- Insert empty lines without moving cursor
+vim.keymap.set("n", "<leader>o", '@="m`o<C-V><Esc>``"<CR>', { desc = "Insert empty line below" })
+vim.keymap.set("n", "<leader>O", '@="m`O<C-V><Esc>``"<CR>', { desc = "Insert empty line above" })
