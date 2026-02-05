ea0f0130073af3e6992d8ad9c5c55ad74b713696
Kristian Holme <holme.kristian@gmail.com>
2026-02-05 13:10:40 +0100

    tweak julia setup

--- bin/julia-setup.jl
+++ bin/julia-setup.jl
@@ -5,7 +5,7 @@ Pkg.activate()
 packages = [
     "Revise", "BenchmarkTools", "Cthulhu", "Debugger", "DrWatson", "JET",
     "PkgTemplates", "ProgressMeter", "OhMyREPL", "Reexport",
-    "Infiltrator", "ArtifactUtils",
+    "Infiltrator", "ArtifactUtils", "ExplicitImports", "PreferenceTools",
 ]
 for p in packages
     try
@@ -16,5 +16,18 @@ for p in packages
     end
 end
 
-Pkg.activate("runic", shared = true)
-Pkg.add("Runic")
+# Install Runic via Apps interface
+try
+    Pkg.Apps.add("Runic")
+    @info "Installed Runic"
+catch e
+    @warn "Error installing Runic" exception = (e, catch_backtrace())
+end
+
+# Install JETLS from GitHub
+try
+    Pkg.Apps.add(; url = "https://github.com/aviatesk/JETLS.jl", rev = "release")
+    @info "Installed JETLS"
+catch e
+    @warn "Error installing JETLS" exception = (e, catch_backtrace())
+end
